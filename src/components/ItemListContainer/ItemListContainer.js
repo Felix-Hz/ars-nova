6eee51c94ed1efc8f8723d39c48c1d2624639490
Felix-Hz <feluhv@outlook.es>
2022-05-22 20:55:10 -0300

    add: html syntax

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -8,14 +8,14 @@ const ItemListContainer = (props) => {
       <div>
         <h1 className="greeting">{props.greeting}</h1>
         <h3 className="subtitle">
-          {props.subtitle} Click
+          {props.subtitle} Click{" "}
           <a
             href="https://www.youtube.com/watch?v=mCdA4bJAGGk"
             style={{ fontWeight: "600" }}
             className="rick-astley"
           >
             here
-          </a>
+          </a>{" "}
           to see more.
         </h3>
       </div>
