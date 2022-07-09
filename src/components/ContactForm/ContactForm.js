e494c7b2154e7a3caddfb8524d78edf5de48283b
Felix-Hz <feluhv@outlook.es>
2022-07-09 18:30:18 -0300

    add: contact form - create order - add to firestore contact data

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,12 +1,26 @@
 import React, { useState, useContext } from "react";
 import CartContext from "../../context/CartContext";
 
-import { Box, Button, Heading, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Flex,
+  Spinner,
+  FormControl,
+  Input,
+  FormLabel,
+} from "@chakra-ui/react";
 
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../services/firebase/index";
 
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
   const [loading, setLoading] = useState(false);
   const { cart, getTotal } = useContext(CartContext);
 
@@ -14,22 +28,79 @@ const ContactForm = () => {
     console.log("Creando orden...");
     const orderObject = {
       buyer: {
-        name: "Felix",
-        email: "adsf@gmail.com",
-        phone: "1154542525",
-        address: "Tacuarembó 2525",
+        name: name,
+        email: email,
+        phone: phone,
+        address: address,
       },
-      // items: cart, // Nested array is here
+      // items: cart, // !!!!!!!!!!!!!!!!  -  Nested array  problem is here
       // total: total,
     };
     const orderCollectionRef = collection(db, "orders");
     addDoc(orderCollectionRef, orderObject).then(({ id }) => {
       // Nested arrays error
-      console.log(id);
+      console.log(`La orden ${id} ha sido creada exitosamente`);
     });
+    console.log(orderObject);
   };
 
-  return <div>ContactForm</div>;
+  return (
+    <Flex width="full" align="center" justifyContent="center">
+      <Box p={150}>
+        <Box m={4} textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+              <Input
+                id="name"
+                name="name"
+                onChange={(event) => setName(event.target.value)}
+                value={name}
+                type="text"
+                placeholder="Type your full name"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input
+                id="email"
+                name="email"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+                type="email"
+                placeholder="email@address.com"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Phone</FormLabel>
+              <Input
+                id="phone"
+                name="phone"
+                onChange={(event) => setPhone(event.target.value)}
+                value={phone}
+                type="number"
+                placeholder="Phone number"
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Address</FormLabel>
+              <Input
+                id="address"
+                name="address"
+                onChange={(event) => setAddress(event.target.value)}
+                value={address}
+                type="text"
+                placeholder="Street, Number, Division"
+              />
+            </FormControl>
+            <Button width="full" mt={8} onClick={() => handleCreateOrder()}>
+              Purchase your order
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
 };
 
 export default ContactForm;
